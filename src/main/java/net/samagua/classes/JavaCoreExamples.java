a6cce7630d5710ade4b03c7d2317d6f99b217338
samagua <samagua@bomberosquito.gob.ec>
2023-07-06 10:26:27 -0500

    initial commit

---
+++ src/main/java/net/samagua/classes/JavaCoreExamples.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.samagua.classes;
+
+/**
+ *
+ * @author samagua
+ */
+public class JavaCoreExamples {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
